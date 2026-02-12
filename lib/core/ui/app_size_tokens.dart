41d0ce987f04199df54c5f2534f3da6ced202a41
MikelNoppo <mikelnoppo03@gmail.com>
2026-02-12 17:47:43 +0800

    feat(ui): add AppSizeTokens for centralized responsive sizing

---
+++ lib/core/ui/app_size_tokens.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AppSizeTokens {
+  static double get space2 => 2.h;
+  static double get space4 => 4.h;
+  static double get space8 => 8.h;
+  static double get space10 => 10.h;
+  static double get space12 => 12.h;
+  static double get space16 => 16.h;
+  static double get space20 => 20.h;
+  static double get space24 => 24.h;
+  static double get space32 => 32.h;
+
+  static double get pagePadding => 20.w;
+  static double get cardPadding => 20.r;
+  static double get itemPadding => 16.r;
+
+  static double get radius8 => 8.r;
+  static double get radius12 => 12.r;
+  static double get radius16 => 16.r;
+  static double get radius24 => 24.r;
+
+  static double get icon16 => 16.sp;
+  static double get icon18 => 18.sp;
+  static double get icon20 => 20.sp;
+  static double get icon22 => 22.sp;
+  static double get icon24 => 24.sp;
+  static double get icon28 => 28.sp;
+
+  static double get text12 => 12.sp;
+  static double get text13 => 13.sp;
+  static double get text14 => 14.sp;
+  static double get text16 => 16.sp;
+  static double get text18 => 18.sp;
+  static double get text20 => 20.sp;
+
+  static double get appBarExpanded => 160.h;
+  static double get appBarExpandedLarge => 182.h;
+}
