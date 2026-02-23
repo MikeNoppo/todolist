85a9d351806a0f07e9de85cf6d81e09337186fe8
MikelNoppo <mikelnoppo03@gmail.com>
2026-02-23 11:41:48 +0800

    feat(app): Presents intervention when a blocked package is detected

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,9 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import 'services/app_blocker_service.dart';
 import 'services/app_logger.dart';
+import 'services/permission_service.dart';
 import 'screens/splash_screen.dart';
 
 void main() {
@@ -32,8 +34,72 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> with WidgetsBindingObserver {
+  static const String _tag = 'MainApp';
+
+  final GlobalKey<NavigatorState> _navigatorKey = GlobalKey<NavigatorState>();
+  bool _isInterventionVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _consumeBlockedPackageAndPresent();
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.resumed) {
+      _consumeBlockedPackageAndPresent();
+    }
+  }
+
+  Future<void> _consumeBlockedPackageAndPresent() async {
+    if (_isInterventionVisible) {
+      return;
+    }
+
+    final blockedPackage = await PermissionService.consumeBlockedPackage();
+    if (blockedPackage == null || blockedPackage.isEmpty) {
+      return;
+    }
+
+    final navigator = _navigatorKey.currentState;
+    if (navigator == null || !mounted) {
+      return;
+    }
+
+    AppLogger.info(
+      _tag,
+      'Presenting intervention for blocked package=$blockedPackage',
+    );
+
+    _isInterventionVisible = true;
+    try {
+      await AppBlockerService.showInterventionScreenWithNavigator(
+        navigator,
+        blockedPackage,
+      );
+    } finally {
+      _isInterventionVisible = false;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,6 +109,7 @@ class MainApp extends StatelessWidget {
       splitScreenMode: true,
       builder: (context, child) {
         return MaterialApp(
+          navigatorKey: _navigatorKey,
           title: 'Todo List',
           debugShowCheckedModeBanner: false,
           theme: ThemeData(
